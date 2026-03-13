fa623025b9549e4ac6556d3831249ddf7b512eff
Mario Hewardt <marioh@microsoft.com>
2026-03-13 11:26:49 -0700

    Update SQLite dependency  (#143)
    * Update sqlite dependency
    
    * Update manifest with new version of sqlite

--- tools/pullsqlite3.sh
+++ tools/pullsqlite3.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-wget https://www.sqlite.org/2019/sqlite-amalgamation-3270200.zip
-unzip sqlite-amalgamation-3270200.zip
-mv sqlite-amalgamation-3270200 sqlite3
-rm sqlite-amalgamation-3270200.zip
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+VENDOR_DIR="$SCRIPT_DIR/../vendor"
+
+# Remove existing sqlite3 vendor directory to ensure no stale files remain
+rm -rf "$VENDOR_DIR/sqlite3"
+
+wget https://www.sqlite.org/2026/sqlite-amalgamation-3510300.zip
+unzip sqlite-amalgamation-3510300.zip
+mv sqlite-amalgamation-3510300 "$VENDOR_DIR/sqlite3"
+rm sqlite-amalgamation-3510300.zip
